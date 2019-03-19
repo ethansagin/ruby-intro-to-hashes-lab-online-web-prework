0732c645569ac884d4db98446852735624640add
Ethan <ethansagin@gmail.com>
2019-03-19 19:09:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,8 +13,9 @@ end
 
 def monopoly_with_second_tier
   monopoly = {}
-  monopoly = {:railroads => {}}
-  monopoly[:railroads] = {:pieces => 4}
+  monopoly = {:railroads => {
+    pieces: 4}
+  }
 end
 
 def monopoly_with_third_tier
